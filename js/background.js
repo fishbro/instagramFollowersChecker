1c35b8df4a3dbc56f46e58f2b57bf03630cdc6d6
fish_bro <yurariba@gmail.com>
2023-02-09 01:00:44 +0300

    check personal followers list if user is private

--- js/background.js
+++ js/background.js
@@ -1,34 +1,44 @@
 import {callApi, options} from "/js/modules/api.js";
 
 const checkFollowers = async () => {
-    const followers = await callApi({count: 200}, "following");
+    const followers = await callApi({count: 200}, "followers");
+    const following = await callApi({count: 200}, "following");
 
+    let progress = 0;
     const usersPromises = [];
-    for (let i = 0; i < followers.users.length; i++) {
-      const user = followers.users[i];
+    for (let i = 0; i < following.users.length; i++) {
+      const user = following.users[i];
       const user_id = user.pk;
 
       usersPromises.push(
-          new Promise((resolve, reject) => {
+          new Promise((resolve, _reject) => {
             setTimeout(resolve, i*500);
-          }).then(() => callApi({count: 3}, "following", user_id))
+          }).then(() => {
+              progress += 100 / following.users.length;
+              console.log(progress);
+              return callApi({count: 3}, "following", user_id)
+          })
       );
     }
 
     Promise.all(usersPromises).then((data) => {
-      followers.users.forEach((user, key) => (user.followed = data[key]));
-      const result = followers.users.reduce((acc, user) => {
+      following.users.forEach((user, key) => (user.followed = data[key]));
+      const result = following.users.reduce((acc, user) => {
         if (user.followed.users.findIndex((u) => u.pk === options.viewerId.toString()) > -1) {
           acc.followed.push(user.username);
-        }else if(user.is_private){ //TODO: check personal followers list if user is private
-          acc.is_private.push(user.username);
+        }else if(user.is_private){
+            if(followers.users.findIndex((u) => u.pk === user.pk.toString()) > -1){
+                acc.followed.push(user.username);
+            }else{
+                acc.is_private.push(user.username);
+            }
         }else{
           acc.not_followed.push(user.username);
         }
         return acc;
       }, {followed: [], not_followed: [], is_private: []});
 
-      console.log({followers, result});
+      console.log({following, result});
     });
 }
 
